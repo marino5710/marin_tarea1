0d23d5240e6c31fb40cf5c43c84d53f79d59d89a
marino5710 <brianmarin4@gmail.com>
2023-07-07 20:06:21 -0600

    se creo el script de js

--- index.php
+++ index.php
@@ -4,9 +4,13 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>INTRODUCCION A JAVASCRIPT</title>
 </head>
 <body>
+    <h1>INTRODUCCION A JS</h1>
+    
+
+    <script src="./script.js"></script>
     
 </body>
 </html>
---
+++ script.js
@@ -0,0 +1,48 @@
+// Undefined
+let undefinedVar;
+console.log(undefinedVar);
+
+var anotherUndefinedVar;
+console.log(anotherUndefinedVar);
+
+// Booleano
+let booleanVar = true;
+console.log(booleanVar);
+
+let anotherBooleanVar = false;
+console.log(anotherBooleanVar);
+
+// Numérico
+let numericVar = 10;
+console.log(numericVar);
+
+let anotherNumericVar = 5;
+console.log(anotherNumericVar);
+
+// Cadenas de texto
+let stringVar = 'Hola';
+console.log(stringVar);
+
+let anotherStringVar = 'Mundo';
+console.log(anotherStringVar);
+
+// Nulos
+let nullVar = null;
+console.log(nullVar);
+
+let anotherNullVar = null;
+console.log(anotherNullVar);
+
+// Objetos
+const objectVar = { nombre: 'Brian', edad: 25 };
+console.log(objectVar);
+
+const anotherObjectVar = { color: 'rojo', tamano: 'peque' };
+console.log(anotherObjectVar);
+
+// Arreglos
+let arrayVar = [1, 2, 3];
+console.log(arrayVar);
+
+let anotherArrayVar = ['carro', 'perro', 'barco'];
+console.log(anotherArrayVar);
